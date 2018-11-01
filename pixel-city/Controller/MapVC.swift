c8039f20dcae84b35fb307d00b6be41df7875e67
Abdelhamid Naeem <abdelhamidnaeem@abdelhamids-mac.local>
2018-11-01 13:56:35 -0700

    Animating photos view and adding spinner programmatically.

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -15,10 +15,17 @@ class MapVC: UIViewController {
 
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullUpView: UIView!
+    @IBOutlet weak var pullUpHeightViewConstraint: NSLayoutConstraint!
     
     var locationManager = CLLocationManager()
     let authorizationStats = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
+    
+    var screenSize = UIScreen.main.bounds
+    
+    var spinner: UIActivityIndicatorView?
+    var progressLabel: UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +42,34 @@ class MapVC: UIViewController {
         mapView.addGestureRecognizer(doubleTap)
     }
     
+    func addSwipe() {
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    func animateViewUp(){
+        pullUpHeightViewConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    @objc func animateViewDown() {
+        pullUpHeightViewConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func addSpinner() {
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
+        spinner?.startAnimating()
+        pullUpView.addSubview(spinner!)
+    }
 
     @IBAction func centerMapBtnPressed(_ sender: Any) {
         if authorizationStats == .authorizedAlways || authorizationStats == .authorizedWhenInUse{
@@ -47,7 +82,7 @@ class MapVC: UIViewController {
 extension MapVC: MKMapViewDelegate {
     func centerMapOnUserLocation(){
         guard let coordinate = locationManager.location?.coordinate else{return}
-        let coordinateRegion = MKCoordinateRegion(center: coordinate,latitudinalMeters: regionRadius * 2.0 , longitudinalMeters: regionRadius * 2.0)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate,regionRadius * 2.0 , regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
     }
 }
@@ -76,10 +111,13 @@ extension MapVC: CLLocationManagerDelegate{
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         centerMapOnUserLocation()
     }
-    
+      
     @objc func dropPin(sender: UITapGestureRecognizer){
         //drop tee pin in the map
         removePin()
+        animateViewUp()
+        addSwipe()
+        addSpinner()
         
         let touchPoint = sender.location(in: mapView)
         let touchCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
@@ -87,7 +125,7 @@ extension MapVC: CLLocationManagerDelegate{
         let annotation = DroppablePin(coordinate: touchCoordinates, identifier: "droppablePin")
         mapView.addAnnotation(annotation)
         
-        let coordinatesRegion = MKCoordinateRegion(center: touchCoordinates, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        let coordinatesRegion = MKCoordinateRegionMakeWithDistance(touchCoordinates, regionRadius * 2.0, regionRadius * 2.0)
     
         mapView.setRegion(coordinatesRegion, animated: true)
     }
