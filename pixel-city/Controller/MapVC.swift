9e07ec083cfb1470a3a87e1b537a0287ad2d20ab
Abdelhamid Naeem <abdelhamidnaeem@abdelhamids-mac.local>
2018-11-03 18:39:08 -0700

    Setting up collectionView, adding images and reloadData.

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -46,7 +46,7 @@ class MapVC: UIViewController {
         collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
         collectionView?.delegate = self
         collectionView?.dataSource = self
-        collectionView?.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         
         pullUpView.addSubview(collectionView!)
     }
@@ -158,6 +158,11 @@ extension MapVC: CLLocationManagerDelegate{
         removrProgressLabel()
         cancelAllSessions()
         
+        imageUrlArray = []
+        imageArray = []
+        
+        collectionView?.reloadData()
+        
         animateViewUp()
         addSwipe()
         addSpinner()
@@ -183,7 +188,7 @@ extension MapVC: CLLocationManagerDelegate{
                     if finished{
                         self.removeSpinner()
                         self.removrProgressLabel()
-                        //reload collectionView
+                        self.collectionView?.reloadData()
                     }
                 })
             }
@@ -199,7 +204,6 @@ extension MapVC: CLLocationManagerDelegate{
     
     
     func retrieveURLs(forAnnotation annotation: DroppablePin, handler: @escaping (_ stats: Bool) -> ()) {
-        imageUrlArray = []
         
         Alamofire.request(flickerUrl(forApiKey: apikey, withAnnotation: annotation, numberOfPhotos: 40)).responseJSON { (response) in
             guard let json = response.result.value as? Dictionary<String, AnyObject> else{ return }
@@ -216,7 +220,6 @@ extension MapVC: CLLocationManagerDelegate{
         }
     }
     func retrieveImages(handler: @escaping (_ status: Bool) -> ()) {
-        imageArray = []
         
         for url in imageUrlArray{
             Alamofire.request(url).responseImage { (response) in
@@ -249,12 +252,16 @@ extension MapVC: UICollectionViewDelegate,UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //number of item in array
-        return 4
+        return imageArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
-        return cell!
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell else{ return UICollectionViewCell() }
+        let imageFromIndex = imageArray[indexPath.row]
+        let imageView = UIImageView(image: imageFromIndex)
+        cell.addSubview(imageView)
+        
+        return cell
     }
     
     
